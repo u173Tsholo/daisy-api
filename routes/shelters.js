f3b7853c751189269432c54ce78adc067f06aae0
arthurmoyo <93607076+arthurmoyo@users.noreply.github.com>
2022-04-07 23:19:04 +0200

    Update code

--- routes/shelters.js
+++ routes/shelters.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { response, request } = require('express');
 const router = express.Router();
 
+
+
 router.get('/', (request, response) => {
     var shelters = [
-        'Doorway to Dignity',
         'Sisters of Mercy Immaculata Hall',
+        'Doorway to Dignity',
         'Lady Athena\'s House',];
 
         response.status(200).json(JSON.stringify(shelters));   
@@ -14,11 +16,7 @@ router.get('/', (request, response) => {
 
 router.get('/map', (request, response) => {
 
-
-    var googleMapsCoordinate = ['https://goo.gl/maps/wV8vdhsktQ1J4hdcA', 'https://goo.gl/maps/PV3qgx1QsqUT3cov7']
-
-    var selectedCoordinate = googleMapsCoordinate[Math.floor(Math.random() * googleMapsCoordinate.length)];
-    response.status(200).json(JSON.stringify(selectedCoordinate));  
+    response.status(200).json(JSON.stringify('https://goo.gl/maps/PV3qgx1QsqUT3cov7'));  
     
 });
 
