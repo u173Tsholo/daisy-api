0db33524444b43a45d9174623d6b69347c3b457a
tsholofelo.gomba <tsholofelo.gomba@dariel.co.za>
2022-06-07 18:19:43 +0200

    Removed secrets

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-module.exports = new Sequelize('d68p1ebbpqu54o', 'pulnpqwjllyzyd', '97669e45006d5374bdc7659afbc5831f559d0bd53436fe3d622a412da4c6d815', {
+module.exports = new Sequelize('a', 'b', 'c', {
     host: 'ec2-52-86-56-90.compute-1.amazonaws.com',
     dialect: 'postgres',
     port: 5432,
